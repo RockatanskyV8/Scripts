9b7403d77fc57bfcf34d504a682f2b50eccd6768
1deterministic <22532656+1deterministic@users.noreply.github.com>
2019-08-14 21:14:09 -0300

    Added experimental i3 install scripts for Debian, Ubuntu version bumped to 18.04.3

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@ osname=$(IFS="=" read -ra ADDR <<< $(cat /etc/os-release | grep PRETTY_NAME); ec
 
 # should match /etc/os-release PRETTY_NAME string
 debian="Debian GNU/Linux 10 (buster)"
-ubuntu="Ubuntu 18.04.2 LTS"
+ubuntu="Ubuntu 18.04.3 LTS"
 fedora="Fedora 30 (Workstation Edition)"
 archlinux="Arch Linux"
 
